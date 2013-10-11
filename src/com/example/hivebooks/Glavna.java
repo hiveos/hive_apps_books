496d071cbfec30d17a322ab097d4a0623a0817c6
Ihsan <x.ihsan@gmail.com>
2013-10-11 20:47:16 +0200

    Implemented action bar functionality.

--- src/com/example/hivebooks/Glavna.java
+++ src/com/example/hivebooks/Glavna.java
@@ -1,27 +1,20 @@
 package com.example.hivebooks;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.ArrayList;
 
-import android.os.Bundle;
-import android.os.Environment;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
-import android.view.View;
-import android.view.View.OnClickListener;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
 
-public class Glavna extends Activity implements OnClickListener{
-	
+public class Glavna extends Activity {
+
 	private Button openButton;
 	private LinearLayout drzacPolica;
 	private LinearLayout polica;
@@ -30,42 +23,54 @@ public class Glavna extends Activity implements OnClickListener{
 	private LayoutParams parametri;
 	public static String sdCard;
 	private String imeFajla;
-	
-	
-	private void inicijaliziraj()
-	{
+
+	private void inicijaliziraj() {
 		sdCard = Environment.getExternalStorageDirectory().toString();
-		policaCounter=0;
-		drzacPolica=(LinearLayout)findViewById(R.id.ShelfHolder);
-		openButton=(Button)findViewById(R.id.button1);
-		openButton.setOnClickListener(this);
+		policaCounter = 0;
+		drzacPolica = (LinearLayout) findViewById(R.id.ShelfHolder);
 		police.clear();
-        drzacPolica.removeAllViews();
+		drzacPolica.removeAllViews();
 	}
-	
-	private void dodajPolicu()
-	{
+
+	private void dodajPolicu() {
 		polica = new LinearLayout(this);
 		polica.setOrientation(LinearLayout.HORIZONTAL);
-        polica.setBackgroundResource(R.drawable.shelf);
-        police.add(polica);
-        policaCounter++;
-        parametri = new LayoutParams(LayoutParams.MATCH_PARENT, 210);
-        polica.setLayoutParams(parametri);
-        drzacPolica.addView(polica);
+		polica.setBackgroundResource(R.drawable.shelf);
+		police.add(polica);
+		policaCounter++;
+		parametri = new LayoutParams(LayoutParams.MATCH_PARENT, 210);
+		polica.setLayoutParams(parametri);
+		drzacPolica.addView(polica);
 	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_glavna);
+
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.glavna, menu);
 		return true;
+
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		switch (item.getItemId()) {
+		case R.id.action_open_pdf:
+			Intent openFileManager = new Intent(Glavna.this, MainActivity.class);
+			Glavna.this.startActivity(openFileManager);
+			break;
+		default:
+			return false;
+
+		}
+
+		return super.onOptionsItemSelected(item);
 	}
 
 	@Override
@@ -73,21 +78,9 @@ public class Glavna extends Activity implements OnClickListener{
 		// TODO Auto-generated method stub
 		super.onResume();
 		inicijaliziraj();
-		
-		for (int i = 0; i < 5; i++)
-            dodajPolicu();
-	}
 
-	@Override
-	public void onClick(View arg0) {
-		// TODO Auto-generated method stub
-		switch(arg0.getId())
-		{
-			case R.id.button1:
-				Intent openFileManager = new Intent(Glavna.this,MainActivity.class);
-				Glavna.this.startActivity(openFileManager);
-				break;
-		}
+		for (int i = 0; i < 5; i++)
+			dodajPolicu();
 	}
 
 }
--- src/com/example/hivebooks/MainActivity.java
+++ src/com/example/hivebooks/MainActivity.java
@@ -10,6 +10,7 @@ import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.app.ActionBar;
 import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.content.DialogInterface;
@@ -23,61 +24,65 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends ListActivity {
-    
-    private List<String> items = null;
-    private File whereToCopy = new File(Environment.getExternalStorageDirectory()+"/HIVE/Books/");
-    public Glavna glavnaObjekt;
-    
-    public void kopirajFajl(File sourceLocation, File targetLocation) throws IOException
-	{		
-		InputStream in = new FileInputStream(sourceLocation);
-        OutputStream out = new FileOutputStream(targetLocation);
 
-        // Copy the bits from instream to outstream
-        byte[] buf = new byte[1024];
-        int len;
-        while ((len = in.read(buf)) > 0) {
-            out.write(buf, 0, len);
-        }
-        in.close();
-        out.close();
+	private List<String> items = null;
+	private File whereToCopy = new File(
+			Environment.getExternalStorageDirectory() + "/HIVE/Books/");
+	public Glavna glavnaObjekt;
+
+	public void kopirajFajl(File sourceLocation, File targetLocation)
+			throws IOException {
+		InputStream in = new FileInputStream(sourceLocation);
+		OutputStream out = new FileOutputStream(targetLocation);
+
+		// Copy the bits from instream to outstream
+		byte[] buf = new byte[1024];
+		int len;
+		while ((len = in.read(buf)) > 0) {
+			out.write(buf, 0, len);
+		}
+		in.close();
+		out.close();
 	}
-    
-    
-    @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
-        setContentView(R.layout.activity_main);
-        if(!whereToCopy.exists()) whereToCopy.mkdirs();
-        getFiles(new File(Environment.getExternalStorageDirectory()+"/").listFiles());
-    }
-    @Override
-    protected void onListItemClick(ListView l, View v, int position, long id){
-        int selectedRow = (int)id;
-        
-        if(selectedRow == 0){
-            getFiles(new File(Environment.getExternalStorageDirectory()+"/").listFiles());
-        }
-        
-        else
-        {
-            File file = new File(items.get(selectedRow));
-            String imeFajla = file.getName().toString();
-            String ekstenzijaFajla = imeFajla.substring((imeFajla.lastIndexOf(".") + 1), imeFajla.length());
-            
-            if(file.isDirectory())
-            {
-                getFiles(file.listFiles());
-            }
-            else if (file.isFile() && ekstenzijaFajla.equals("pdf"))
-            {
-            	File bookRoot = new File(file.getPath());
-            	File targetLocation = new File (Glavna.sdCard + "/HIVE/Books/"+imeFajla);
-            	
-            	if(!bookRoot.toString().equals(targetLocation.toString()))
-            	{
-            	
-	            	if(!targetLocation.exists()){
+
+	@Override
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+		setContentView(R.layout.activity_main);
+		if (!whereToCopy.exists())
+			whereToCopy.mkdirs();
+		getFiles(new File(Environment.getExternalStorageDirectory() + "/")
+				.listFiles());
+
+		ActionBar actionBar = getActionBar();
+		actionBar.setDisplayHomeAsUpEnabled(true);
+	}
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		int selectedRow = (int) id;
+
+		if (selectedRow == 0) {
+			getFiles(new File(Environment.getExternalStorageDirectory() + "/")
+					.listFiles());
+		}
+
+		else {
+			File file = new File(items.get(selectedRow));
+			String imeFajla = file.getName().toString();
+			String ekstenzijaFajla = imeFajla.substring(
+					(imeFajla.lastIndexOf(".") + 1), imeFajla.length());
+
+			if (file.isDirectory()) {
+				getFiles(file.listFiles());
+			} else if (file.isFile() && ekstenzijaFajla.equals("pdf")) {
+				File bookRoot = new File(file.getPath());
+				File targetLocation = new File(Glavna.sdCard + "/HIVE/Books/"
+						+ imeFajla);
+
+				if (!bookRoot.toString().equals(targetLocation.toString())) {
+
+					if (!targetLocation.exists()) {
 						try {
 							targetLocation.createNewFile();
 						} catch (IOException e1) {
@@ -85,50 +90,52 @@ public class MainActivity extends ListActivity {
 							e1.printStackTrace();
 						}
 					}
-	            	
-	            	try {
+
+					try {
 						kopirajFajl(bookRoot, targetLocation);
 					} catch (IOException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-	            	
-	            	Intent intent = new Intent(Intent.ACTION_VIEW);
-	            	intent.setDataAndType(Uri.fromFile(targetLocation),"application/pdf");
-	            	intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-	            	startActivity(intent);
-            	}
-            	
-            	if(bookRoot.toString().equals(targetLocation.toString()))
-            	{
-            		Intent intent = new Intent(Intent.ACTION_VIEW);
-	            	intent.setDataAndType(Uri.fromFile(targetLocation),"application/pdf");
-	            	intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-	            	startActivity(intent);
-            	}
-            	
-            }
-            else
-            {
-                 new AlertDialog.Builder(this)
-                 .setTitle(ekstenzijaFajla + " files are not supported ")
-                 .setNeutralButton("OK", new DialogInterface.OnClickListener(){
-                     public void onClick(DialogInterface dialog, int button){
-                         //do nothing
-                     }
-                 })
-                 .show();
-            }
-            
-        }
-    }
-    private void getFiles(File[] files){
-        items = new ArrayList<String>();
-        items.add("Go back");
-        for(File file : files){
-            items.add(file.getPath());
-        }
-        ArrayAdapter<String> fileList = new ArrayAdapter<String>(this,R.layout.file_list_row, items);
-        setListAdapter(fileList);
-    }
+
+					Intent intent = new Intent(Intent.ACTION_VIEW);
+					intent.setDataAndType(Uri.fromFile(targetLocation),
+							"application/pdf");
+					intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+					startActivity(intent);
+				}
+
+				if (bookRoot.toString().equals(targetLocation.toString())) {
+					Intent intent = new Intent(Intent.ACTION_VIEW);
+					intent.setDataAndType(Uri.fromFile(targetLocation),
+							"application/pdf");
+					intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+					startActivity(intent);
+				}
+
+			} else {
+				new AlertDialog.Builder(this)
+						.setTitle(ekstenzijaFajla + " files are not supported ")
+						.setNeutralButton("OK",
+								new DialogInterface.OnClickListener() {
+									public void onClick(DialogInterface dialog,
+											int button) {
+										// do nothing
+									}
+								}).show();
+			}
+
+		}
+	}
+
+	private void getFiles(File[] files) {
+		items = new ArrayList<String>();
+		items.add("Go back");
+		for (File file : files) {
+			items.add(file.getPath());
+		}
+		ArrayAdapter<String> fileList = new ArrayAdapter<String>(this,
+				R.layout.file_list_row, items);
+		setListAdapter(fileList);
+	}
 }
