2093c112eb152e13df99a3ab45d6fa04605ab78c
Ihsan Isik <x.ihsan@gmail.com>
2014-01-03 15:41:32 +0100

    UI redesign, immersive mode (half-done), some small adjustments and fixations...

--- src/hive/apps/books/Glavna.java
+++ src/hive/apps/books/Glavna.java
@@ -2,25 +2,39 @@ package hive.apps.books;
 
 import java.io.File;
 import java.util.ArrayList;
+
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnLongClickListener;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
-import android.widget.Toast;
+import android.widget.TextView;
 
-public class Glavna extends Activity implements OnClickListener, OnLongClickListener{
+public class Glavna extends Activity implements OnClickListener,
+		OnLongClickListener {
 
 	private Button openButton;
 	private LinearLayout drzacPolica;
@@ -29,13 +43,13 @@ public class Glavna extends Activity implements OnClickListener, OnLongClickList
 	private ArrayList<MyButton> knjige = new ArrayList<MyButton>();
 	public static ArrayList<Bitmap> nizStranica = new ArrayList<Bitmap>();
 	private int policaCounter;
-	private LayoutParams parametri;
+	private LayoutParams params;
 	private LayoutParams knjigaParams;
 	public static String sdCard;
 	private String imeFajla;
 	private int knjigaCounter;
 	private MyButton knjiga;
-	private Boolean isNeededToLoad=false;
+	private Boolean isNeededToLoad = false;
 	private int ukupniKnjigaCounter;
 	private int policaNaKojojSeNalazimo;
 	private int brojKnjigaZaLoadati;
@@ -44,99 +58,24 @@ public class Glavna extends Activity implements OnClickListener, OnLongClickList
 	public static String obradjujemo = "";
 	public static File path;
 	public File[] stranice;
+	private LinearLayout emptyspace;
 
-	private void inicijaliziraj() {
-		sdCard = Environment.getExternalStorageDirectory().toString();
-		policaCounter = 0;
-		drzacPolica = (LinearLayout) findViewById(R.id.ShelfHolder);
-		police.clear();
-		knjige.clear();
-		drzacPolica.removeAllViews();
-		policaCounter = 0;
-        knjigaCounter = 0;
-        policaNaKojojSeNalazimo = 0;
-        ukupniKnjigaCounter = 0;
-        nizStranica.clear();
-        
-        File sec = new File(Environment.getExternalStorageDirectory()+"/HIVE/Books/");
-        if(!sec.exists()) sec.mkdirs();
-        
-        brojKnjigaZaLoadati = (new File(
-                Environment.getExternalStorageDirectory() + "/HIVE/Books/")
-                .listFiles().length);
-	}
+	View selectedItem;
 
-	private void dodajPolicu() {
-		polica = new LinearLayout(this);
-		polica.setOrientation(LinearLayout.HORIZONTAL);
-		polica.setBackgroundResource(R.drawable.shelf);
-		police.add(polica);
-		policaCounter++;
-		parametri = new LayoutParams(LayoutParams.MATCH_PARENT, 210);
-		polica.setLayoutParams(parametri);
-		drzacPolica.addView(polica);
-	}
-	
-	private void dodajKnjigu(){
-		if(knjigaCounter<3)
-		{
-			Log.d("Ime foldera:", foldernoIme);
-			knjiga=new MyButton(this, foldernoIme);
-			knjiga.setOnClickListener(this);
-			knjiga.setOnLongClickListener(this);
-			knjiga.setId(ukupniKnjigaCounter);
-			String imeKnjigeBezEkstenzije = knjiga.imeKnjige;
-			Log.d("IME KNJIGE BEZ EKSTENZIJE", imeKnjigeBezEkstenzije);
-			
-			BitmapFactory.Options options = new BitmapFactory.Options();
-			options.inPreferredConfig = Bitmap.Config.ARGB_8888;
-			Bitmap bitmap = BitmapFactory.decodeFile(sdCard + "/HIVE/Books/"
-					+ foldernoIme + "/1.jpg", options);	
-			Bitmap smanjeni = bitmap.createScaledBitmap(bitmap, 130, 170, false);
-			
-			knjiga.setImageBitmap(smanjeni);
-			knjige.add(knjiga);
-			knjigaCounter++;
-			Log.d("Knjiga counter:" , knjigaCounter+"");
-			ukupniKnjigaCounter++;
-			knjigaParams = new LayoutParams(LayoutParams.WRAP_CONTENT,
-                    LayoutParams.WRAP_CONTENT);
-			knjigaParams.leftMargin = 30;
-            knjigaParams.bottomMargin = 20;
-            knjigaParams.topMargin = 15;
-            police.get(policaNaKojojSeNalazimo).addView(
-                    knjige.get(ukupniKnjigaCounter - 1), knjigaParams);
-		}
-		else if(knjigaCounter>=3)
-		{
-			if (ukupniKnjigaCounter >= 20)
-                dodajPolicu();
-				policaNaKojojSeNalazimo++;
-				knjigaCounter=0;
-				dodajKnjigu();
-		}
-	}
-	
-	private void loadajKnjige() {
-		// TODO Auto-generated method stub
-		brojKnjizica = new File(Environment.getExternalStorageDirectory()
-                + "/HIVE/Books/").listFiles();
-		
-		for (File infile : brojKnjizica) {
-            if (infile.isDirectory()) {
-            		foldernoIme = infile.getName();
-                    isNeededToLoad = true;
-                    dodajKnjigu();
-            }
-		}
-		
-	}
+	private int ItemId;
+	protected Object mActionMode;
+
+	public static final String SHELF_STYLE = "shelfstyle";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_glavna);
 
+		File BooksDir = new File(Environment.getExternalStorageDirectory()
+				+ "/HIVE/Books");
+		if (!BooksDir.exists())
+			BooksDir.mkdirs();
 	}
 
 	@Override
@@ -150,9 +89,11 @@ public class Glavna extends Activity implements OnClickListener, OnLongClickList
 	public boolean onOptionsItemSelected(MenuItem item) {
 
 		switch (item.getItemId()) {
-		case R.id.action_open_pdf:
-			Intent openFileManager = new Intent(Glavna.this, MainActivity.class);
-			Glavna.this.startActivity(openFileManager);
+
+		case R.id.action_settings:
+			Intent goToSettings = new Intent(Glavna.this,
+					SettingsActivity.class);
+			Glavna.this.startActivity(goToSettings);
 			break;
 		default:
 			return false;
@@ -164,15 +105,132 @@ public class Glavna extends Activity implements OnClickListener, OnLongClickList
 
 	@Override
 	protected void onResume() {
-		// TODO Auto-generated method stub
 		super.onResume();
 		inicijaliziraj();
 
 		for (int i = 0; i < 5; i++)
 			dodajPolicu();
-		
+
 		if (brojKnjigaZaLoadati != 0)
-            loadajKnjige();
+			loadajKnjige();
+	}
+
+	private void inicijaliziraj() {
+		sdCard = Environment.getExternalStorageDirectory().toString();
+		policaCounter = 0;
+		drzacPolica = (LinearLayout) findViewById(R.id.ShelfHolder);
+		police.clear();
+		knjige.clear();
+		drzacPolica.removeAllViews();
+		policaCounter = 0;
+		knjigaCounter = 0;
+		policaNaKojojSeNalazimo = 0;
+		ukupniKnjigaCounter = 0;
+		nizStranica.clear();
+
+		File booksRoot = new File(Environment.getExternalStorageDirectory()
+				+ "/HIVE/Books/");
+		if (!booksRoot.exists())
+			booksRoot.mkdirs();
+
+		brojKnjigaZaLoadati = (new File(
+				Environment.getExternalStorageDirectory() + "/HIVE/Books/")
+				.listFiles().length);
+	}
+
+	private void dodajPolicu() {
+		emptyspace = (LinearLayout) findViewById(R.id.space);
+		polica = new LinearLayout(this);
+		polica.setOrientation(LinearLayout.HORIZONTAL);
+		police.add(polica);
+		policaCounter++;
+		params = new LayoutParams(LayoutParams.MATCH_PARENT,
+				LayoutParams.WRAP_CONTENT);
+		polica.setLayoutParams(params);
+		polica.setGravity(Gravity.CENTER_HORIZONTAL);
+		emptyspace.setBackgroundResource(R.drawable.transparent);
+
+		if (getShelfStyle().equals("no")) {
+			params.topMargin = 55;
+			params.leftMargin = 50;
+			params.rightMargin = 50;
+			emptyspace.setBackgroundResource(R.drawable.transparent);
+		}
+		if (getShelfStyle().equals("simple")) {
+			params.topMargin = 55;
+			params.leftMargin = 50;
+			params.rightMargin = 50;
+			polica.setBackgroundResource(R.drawable.shelf_simple);
+			emptyspace.setBackgroundResource(R.drawable.transparent);
+		}
+		if (getShelfStyle().equals("wooden")) {
+			params.topMargin = 0;
+			params.leftMargin = 0;
+			params.rightMargin = 0;
+			polica.setPadding(50, 20, 50, 0);
+			polica.setBackgroundResource(R.drawable.shelf_wooden);
+			emptyspace.setBackgroundResource(R.drawable.shelf_wooden_empty);
+		}
+
+		drzacPolica.addView(polica);
+	}
+
+	public String getShelfStyle() {
+		SharedPreferences settings = PreferenceManager
+				.getDefaultSharedPreferences(this);
+		return settings.getString(SHELF_STYLE, "");
+	}
+
+	private void dodajKnjigu() {
+		if (knjigaCounter < 3) {
+			Log.d("Ime foldera:", foldernoIme);
+			knjiga = new MyButton(this, foldernoIme);
+			knjiga.setOnClickListener(this);
+			knjiga.setOnLongClickListener(this);
+			knjiga.setId(ukupniKnjigaCounter);
+			String imeKnjigeBezEkstenzije = knjiga.imeKnjige;
+			Log.d("IME KNJIGE BEZ EKSTENZIJE", imeKnjigeBezEkstenzije);
+
+			BitmapFactory.Options options = new BitmapFactory.Options();
+			options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+			Bitmap bitmap = BitmapFactory.decodeFile(sdCard + "/HIVE/Books/"
+					+ foldernoIme + "/1.jpg", options);
+			Bitmap smanjeni = Bitmap
+					.createScaledBitmap(bitmap, 130, 170, false);
+
+			knjiga.setImageBitmap(smanjeni);
+			knjige.add(knjiga);
+			knjigaCounter++;
+			Log.d("Knjiga counter:", knjigaCounter + "");
+			ukupniKnjigaCounter++;
+			knjigaParams = new LayoutParams(LayoutParams.WRAP_CONTENT,
+					LayoutParams.WRAP_CONTENT);
+			knjigaParams.leftMargin = 30;
+			knjigaParams.bottomMargin = 20;
+			knjigaParams.topMargin = 15;
+			police.get(policaNaKojojSeNalazimo).addView(
+					knjige.get(ukupniKnjigaCounter - 1), knjigaParams);
+		} else if (knjigaCounter >= 3) {
+			if (ukupniKnjigaCounter >= 20)
+				dodajPolicu();
+			policaNaKojojSeNalazimo++;
+			knjigaCounter = 0;
+			dodajKnjigu();
+		}
+	}
+
+	private void loadajKnjige() {
+		brojKnjizica = new File(Environment.getExternalStorageDirectory()
+				+ "/HIVE/Books/").listFiles();
+
+		for (File infile : brojKnjizica) {
+			if (infile.isDirectory()) {
+				foldernoIme = infile.getName();
+				isNeededToLoad = true;
+				dodajKnjigu();
+			}
+		}
+
 	}
 
 	@Override
@@ -180,37 +238,39 @@ public class Glavna extends Activity implements OnClickListener, OnLongClickList
 		// TODO Auto-generated method stub
 		return false;
 	}
-	
-	public void ucitajStranice(){
+
+	public void ucitajStranice() {
 		stranice = path.listFiles();
-		System.out.println(stranice.length+"");
-		
+		System.out.println(stranice.length + "");
+
 		for (File stranica : stranice) {
-            if (stranica.isFile()) {
-            	Bitmap bmpStranica = BitmapFactory.decodeFile(stranica.getAbsolutePath());
-            	nizStranica.add(bmpStranica);
-            }
+			if (stranica.isFile()) {
+				Bitmap bmpStranica = BitmapFactory.decodeFile(stranica
+						.getAbsolutePath());
+				nizStranica.add(bmpStranica);
+			}
 		}
-		System.out.println(nizStranica.size()+" ");
+		System.out.println(nizStranica.size() + " ");
 	}
 
 	@Override
 	public void onClick(View arg0) {
 		// TODO Auto-generated method stub
-		MyButton trenutno=(MyButton)arg0;
-		for(int i=0; i<knjige.size(); i++)
-		{
-			if(knjige.get(i).imeKnjige.toString().equals(trenutno.imeKnjige.toString())){
-				obradjujemo=knjige.get(i).imeKnjige.toString();
+		MyButton trenutno = (MyButton) arg0;
+		for (int i = 0; i < knjige.size(); i++) {
+			if (knjige.get(i).imeKnjige.toString().equals(
+					trenutno.imeKnjige.toString())) {
+				obradjujemo = knjige.get(i).imeKnjige.toString();
 			}
 		}
-		path = new File(sdCard + "/HIVE/Books/"
-				+ obradjujemo+"/");
+		path = new File(Environment.getExternalStorageDirectory()
+				+ "/HIVE/Books/" + obradjujemo + "/");
 		Log.d("Obradjujemo:", path.getAbsolutePath().toString());
 		ucitajStranice();
 		Intent intent = new Intent(Glavna.this, PageScroller.class);
-        startActivity(intent);
-		
+		startActivity(intent);
+
 	}
+
 
 }
--- src/hive/apps/books/PageScroller.java
+++ src/hive/apps/books/PageScroller.java
@@ -1,11 +1,10 @@
 package hive.apps.books;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
-
 import hive.apps.books.R.id;
+import android.os.Build;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
@@ -23,59 +22,133 @@ import android.widget.Toast;
 import android.widget.ViewFlipper;
 
 public class PageScroller extends Activity {
-	
+
 	ViewFlipper viewFlipper;
 	public Boolean isEditable;
 	CrtanjeView crtanjeView;
 	Bitmap bip;
-	int brr=0;
+	int brr = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_page_scroller);
+
+		 final View decorView = getWindow().getDecorView();
+		 decorView
+		 .setOnSystemUiVisibilityChangeListener(new
+		 View.OnSystemUiVisibilityChangeListener() {
+		 @Override
+		 public void onSystemUiVisibilityChange(int i) {
+		 int height = decorView.getHeight();
+		 Log.i("TAG", "Current height: " + height);
+		 }
+		 });
+		
+		 toggleHideyBar();
 	}
+
 	
+	 public void toggleHideyBar() {
 	
+	 // The UI options currently enabled are represented by a bitfield.
+	 // getSystemUiVisibility() gives us that bitfield.
+	 int uiOptions = getWindow().getDecorView().getSystemUiVisibility();
+	 int newUiOptions = uiOptions;
+	 boolean isImmersiveModeEnabled = ((uiOptions |
+	 View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY) == uiOptions);
+	 if (isImmersiveModeEnabled) {
+	 Log.i("TAG", "Turning immersive mode mode off. ");
+	 } else {
+	 Log.i("TAG", "Turning immersive mode mode on.");
+	 }
 	
+	 // Navigation bar hiding: Backwards compatible to ICS.
+	 if (Build.VERSION.SDK_INT >= 14) {
+	 newUiOptions ^= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+	 }
+	
+	 // Status bar hiding: Backwards compatible to Jellybean
+	 if (Build.VERSION.SDK_INT >= 16) {
+	 newUiOptions ^= View.SYSTEM_UI_FLAG_FULLSCREEN;
+	 }
+	
+	 // Immersive mode: Backward compatible to KitKat.
+	 // Note that this flag doesn't do anything by itself, it only augments
+	 // the behavior
+	 // of HIDE_NAVIGATION and FLAG_FULLSCREEN. For the purposes of this
+	 // sample
+	 // all three flags are being toggled together.
+	 // Note that there are two immersive mode UI flags, one of which is
+	 // referred to as "sticky".
+	 // Sticky immersive mode differs in that it makes the navigation and
+	 // status bars
+	 // semi-transparent, and the UI flag does not get cleared when the user
+	 // interacts with
+	 // the screen.
+	 if (Build.VERSION.SDK_INT >= 18) {
+	 newUiOptions ^= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+	 }
+	
+	 getWindow().getDecorView()
+	 .setSystemUiVisibility(newUiOptions);
+	 }
+
 	@Override
 	protected void onResume() {
 		// TODO Auto-generated method stub
 		super.onResume();
-		isEditable=false;
+		isEditable = false;
 		viewFlipper = (ViewFlipper) findViewById(R.id.view_flipper);
-		crtanjeView=(CrtanjeView)findViewById(R.id.crtanje);
+		crtanjeView = (CrtanjeView) findViewById(R.id.crtanje);
 		postaviViewFlipper();
 	}
 
+	// @Override
+	// public void onWindowFocusChanged(boolean hasFocus) {
+	// super.onWindowFocusChanged(hasFocus);
+	// if (hasFocus) {
+	// this.getWindow()
+	// .getDecorView()
+	// .setSystemUiVisibility(
+	// View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+	// | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+	// | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+	// | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+	// | View.SYSTEM_UI_FLAG_FULLSCREEN
+	// | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+	// }
+	// }
 
-
-	public void postaviViewFlipper(){
+	public void postaviViewFlipper() {
 		LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		brr=0;
+		brr = 0;
 		for (int i = 0; i < Glavna.nizStranica.size(); i++) {
 			View view = inflater.inflate(R.layout.flip_lay, viewFlipper, false);
-			ImageView slika = (ImageView)view.findViewById(R.id.slikaStranice);
+			ImageView slika = (ImageView) view.findViewById(R.id.slikaStranice);
 			bip = Glavna.nizStranica.get(i);
 			slika.setImageBitmap(bip);
 			viewFlipper.addView(view);
 			slika.setOnTouchListener(new OnSwipeTouchListener() {
-			    public void onSwipeTop() {
-			    }
-			    public void onSwipeRight() {
-			    	desno();
-			    }
-			    public void onSwipeLeft() {
-			    	lijevo();
-			    }
-			    public void onSwipeBottom() {
-			    }
+				public void onSwipeTop() {
+				}
+
+				public void onSwipeRight() {
+					desno();
+				}
+
+				public void onSwipeLeft() {
+					lijevo();
+				}
+
+				public void onSwipeBottom() {
+				}
 			});
 		}
 	}
-	
-	public void lijevo(){
-		if(!isEditable){
+
+	public void lijevo() {
+		if (!isEditable) {
 			brr++;
 			crtanjeView.ocistiFunkcija();
 			viewFlipper.setOutAnimation(this, R.anim.out_to_left);
@@ -83,11 +156,13 @@ public class PageScroller extends Activity {
 			viewFlipper.showNext();
 		}
 	}
-	
-	public void desno(){
-		if(brr>0) brr--;
-		else brr=Glavna.nizStranica.size();
-		if(!isEditable){
+
+	public void desno() {
+		if (brr > 0)
+			brr--;
+		else
+			brr = Glavna.nizStranica.size();
+		if (!isEditable) {
 			crtanjeView.ocistiFunkcija();
 			viewFlipper.setOutAnimation(this, R.anim.out_to_right);
 			viewFlipper.setInAnimation(this, R.anim.in_from_left);
@@ -106,7 +181,7 @@ public class PageScroller extends Activity {
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// TODO Auto-generated method stub
-		switch(item.getItemId()){
+		switch (item.getItemId()) {
 		case R.id.previous:
 			viewFlipper.setOutAnimation(this, R.anim.out_to_right);
 			viewFlipper.setInAnimation(this, R.anim.in_from_left);
@@ -118,16 +193,16 @@ public class PageScroller extends Activity {
 			viewFlipper.showNext();
 			return true;
 		case R.id.edit:
-			if(isEditable==true){
-				isEditable=false;
+			if (isEditable == true) {
+				isEditable = false;
 				findViewById(R.id.next).setEnabled(true);
 				findViewById(R.id.previous).setEnabled(true);
-				crtanjeView.spreminamStranicu(brr+1);
+				crtanjeView.spreminamStranicu(brr + 1);
 				postaviViewFlipper();
 				return true;
 			}
-			if(!isEditable){
-				isEditable=true;
+			if (!isEditable) {
+				isEditable = true;
 				findViewById(R.id.next).setEnabled(false);
 				findViewById(R.id.previous).setEnabled(false);
 				crtanjeView.bringToFront();
@@ -135,11 +210,10 @@ public class PageScroller extends Activity {
 				return false;
 			}
 			return true;
-		default: return false;
-		
+		default:
+			return false;
+
 		}
 	}
-	
-	
 
 }
