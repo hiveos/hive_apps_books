88a88d77860da9a28fa67d94a4b65bd4e3274e29
Ihsan Isik <x.ihsan@gmail.com>
2014-01-04 10:17:49 +0100

    Action bar icons and immersive mode on item click

--- src/hive/apps/books/PageScroller.java
+++ src/hive/apps/books/PageScroller.java
@@ -1,10 +1,13 @@
 package hive.apps.books;
 
 import uk.co.senab.photoview.PhotoViewAttacher;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,6 +16,8 @@ import android.widget.ImageView;
 import android.widget.ViewFlipper;
 
 public class PageScroller extends Activity {
+
+	private Menu menu;
 
 	ViewFlipper viewFlipper;
 	public Boolean isEditable;
@@ -82,6 +87,7 @@ public class PageScroller extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
+		this.menu = menu;
 		getMenuInflater().inflate(R.menu.page_scroller, menu);
 		setTitle(Glavna.obradjujemo);
 		return true;
@@ -89,34 +95,50 @@ public class PageScroller extends Activity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
+
+		MenuItem fullscreenItem = menu.findItem(R.id.action_fullscreen);
+		
+		int uiOptions = getWindow().getDecorView().getSystemUiVisibility();
+		int newUiOptions = uiOptions;
+		boolean isImmersiveModeEnabled = ((uiOptions | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY) == uiOptions);
+
 		switch (item.getItemId()) {
-		case R.id.previous:
+		case R.id.action_previous:
 			viewFlipper.setOutAnimation(this, R.anim.out_to_right);
 			viewFlipper.setInAnimation(this, R.anim.in_from_left);
 			viewFlipper.showPrevious();
 			return true;
-		case R.id.next:
+		case R.id.action_next:
 			viewFlipper.setOutAnimation(this, R.anim.out_to_left);
 			viewFlipper.setInAnimation(this, R.anim.in_from_right);
 			viewFlipper.showNext();
 			return true;
-		case R.id.edit:
+		case R.id.action_edit:
 			if (isEditable == true) {
 				isEditable = false;
-				findViewById(R.id.next).setEnabled(true);
-				findViewById(R.id.previous).setEnabled(true);
+				findViewById(R.id.action_next).setEnabled(true);
+				findViewById(R.id.action_previous).setEnabled(true);
 				crtanjeView.spreminamStranicu(brr + 1);
 				postaviViewFlipper();
 				return true;
 			}
 			if (!isEditable) {
 				isEditable = true;
-				findViewById(R.id.next).setEnabled(false);
-				findViewById(R.id.previous).setEnabled(false);
+				findViewById(R.id.action_next).setEnabled(false);
+				findViewById(R.id.action_previous).setEnabled(false);
 				crtanjeView.bringToFront();
 				crtanjeView.nacrtajnamCanvas(bip);
 				return false;
 			}
+		case R.id.action_fullscreen:
+			if (!isImmersiveModeEnabled) {
+				fullscreenItem.setIcon(R.drawable.ic_navigation_expand);
+				toggleImmersive();
+			} else if (isImmersiveModeEnabled) {
+				fullscreenItem.setIcon(R.drawable.ic_navigation_collapse);
+				toggleImmersive();
+			}
+
 			return true;
 		default:
 			return false;
@@ -124,4 +146,33 @@ public class PageScroller extends Activity {
 		}
 	}
 
+	public void toggleImmersive() {
+
+		int uiOptions = getWindow().getDecorView().getSystemUiVisibility();
+		int newUiOptions = uiOptions;
+		boolean isImmersiveModeEnabled = ((uiOptions | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY) == uiOptions);
+		if (isImmersiveModeEnabled) {
+			Log.i("TAG", "Turning immersive mode mode off. ");
+			getActionBar()
+					.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+			getActionBar().show();
+		} else {
+			Log.i("TAG", "Turning immersive mode mode on.");
+			getActionBar().show();
+		}
+
+		if (Build.VERSION.SDK_INT >= 14) {
+			newUiOptions ^= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+		}
+
+		if (Build.VERSION.SDK_INT >= 16) {
+			newUiOptions ^= View.SYSTEM_UI_FLAG_FULLSCREEN;
+		}
+
+		if (Build.VERSION.SDK_INT >= 18) {
+			newUiOptions ^= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+		}
+
+		getWindow().getDecorView().setSystemUiVisibility(newUiOptions);
+	}
 }
