9526c7040a3db1ddddc558593a32585903a6ef74
Ihsan <x.ihsan@gmail.com>
2014-04-10 18:35:15 +0200

    Add Android Studio support

---
+++ Books/src/hive/apps/books/mojaPutanja.java
@@ -0,0 +1,12 @@
+package hive.apps.books;
+
+import android.graphics.Paint;
+import android.graphics.Path;
+
+public class mojaPutanja extends Path {
+	public Paint bojaPutanje;
+
+	public mojaPutanja(Paint boja) {
+		bojaPutanje = boja;
+	}
+}
